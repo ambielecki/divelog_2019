f36f5bf7b3a32b627fffc01bbf10d87f4be53d8b
Andrew Bielecki <andrew.bielecki@healthcaresource.com>
2019-02-11 08:33:42 -0500

    add a row to fix navbar

--- resources/views/main/header/header.blade.php
+++ resources/views/main/header/header.blade.php
@@ -22,18 +22,22 @@
 {{-- Main Nav Skeleton --}}
 <nav>
     <div class="nav-wrapper">
-        <a href="#" class="brand-logo">DiveLogRepeat</a>
-        <a href="#" data-target="mobile_nav" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="#">Calculator</a></li>
-            <li><a href="#">Dive Log</a></li>
-            <li><a href="">Blog</a></li>
-            @if (Auth::check())
-                <li><a class="dropdown-trigger" href="#!" data-target="logout_dropdown">Welcome {{ auth()->user()->first_name }}<i class="material-icons right">arrow_drop_down</i></a></li>
-            @else
-                <li><a class="dropdown-trigger" href="#!" data-target="login_dropdown">Login<i class="material-icons right">arrow_drop_down</i></a></li>
-            @endif
-        </ul>
+        <div class="row">
+            <div class="col s12">
+                <a href="#" class="brand-logo">DiveLogRepeat</a>
+                <a href="#" data-target="mobile_nav" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="#">Calculator</a></li>
+                    <li><a href="#">Dive Log</a></li>
+                    <li><a href="">Blog</a></li>
+                    @if (Auth::check())
+                        <li><a class="dropdown-trigger" href="#!" data-target="logout_dropdown">Welcome {{ auth()->user()->first_name }}<i class="material-icons right">arrow_drop_down</i></a></li>
+                    @else
+                        <li><a class="dropdown-trigger" href="#!" data-target="login_dropdown">Login<i class="material-icons right">arrow_drop_down</i></a></li>
+                    @endif
+                </ul>
+            </div>
+        </div>
     </div>
 </nav>
 
--- resources/views/main/home/index.blade.php
+++ resources/views/main/home/index.blade.php
@@ -2,14 +2,16 @@
 
 @section('content')
     <div class="row">
-        <div class="col m6 s12">
+        <div class="col m4 s12">
             @if (!empty($page['content']['carousel_images']))
-                <div class="carousel carousel-slider center">
-                    @foreach ($page['content']['carousel_images'] as $image)
-                        <a class="carousel-item" href="{{ $image['path'] }}">
-                            <img src="{{ $image['path'] }}" alt="{{ $image['caption'] ?? 'DiveLogRepeat' }}">
-                        </a>
-                    @endforeach
+                <div class="card">
+                    <div class="carousel carousel-slider center">
+                        @foreach ($page['content']['carousel_images'] as $image)
+                            <a class="carousel-item" href="{{ $image['path'] }}">
+                                <img src="{{ $image['path'] }}" alt="{{ $image['caption'] ?? 'DiveLogRepeat' }}" title="{{ $image['title'] }}">
+                            </a>
+                        @endforeach
+                    </div>
                 </div>
             @endif
         </div>
