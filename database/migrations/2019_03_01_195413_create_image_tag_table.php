969cf5e289f6509c2d76ba9acabda0c364348755
Andrew Bielecki <andrew.bielecki@healthcaresource.com>
2019-03-01 15:14:25 -0500

    Image Table Migrations

---
+++ database/migrations/2019_03_01_195413_create_image_tag_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImageTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('image_tag', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('image_id');
+            $table->unsignedBigInteger('tag_id');
+            $table->timestamps();
+            $table->foreign('image_id')->references('id')->on('images');
+            $table->foreign('tag_id')->references('id')->on('tags');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('image_tag');
+    }
+}
