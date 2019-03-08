698b3cf9e676c353092187c4b6ded46439ee9264
Andrew Bielecki <andrew.bielecki@healthcaresource.com>
2019-03-08 13:37:13 -0500

    revert

--- app/Models/Pages/BlogPage.php
+++ app/Models/Pages/BlogPage.php
@@ -54,12 +54,7 @@ class BlogPage extends Page {
     }
 
     public static function checkSlug(string $slug): bool {
-        return self::query()
-            ->where([
-                ['slug', $slug],
-                ['is_active', 1]
-            ])
-            ->count() === 0;
+        return self::where('slug', $slug)->count() === 0;
     }
 
     public static function getImageIds($content): array {
