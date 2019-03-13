61fbaf068a525e65ee52809ef0e42a97c984347d
Andrew Bielecki <andrew.bielecki@healthcaresource.com>
2019-03-13 15:21:16 -0400

    Blog route to main header

--- resources/views/main/header/header.blade.php
+++ resources/views/main/header/header.blade.php
@@ -38,7 +38,7 @@
                     <ul id="nav-mobile" class="right hide-on-med-and-down">
                         <li><a href="{{ route('calculator') }}">Calculator</a></li>
                         <li><a href="#">Dive Log</a></li>
-                        <li><a href="">Blog</a></li>
+                        <li><a href="{{ route('blog_list') }}">Blog</a></li>
                         @admin
                         <li><a class="dropdown-trigger" href="#!" data-target="admin_dropdown">Admin<i class="material-icons right">arrow_drop_down</i></a></li>
                         @endadmin
@@ -58,7 +58,7 @@
 <ul class="sidenav" id="mobile_nav">
     <li><a href="{{ route('calculator') }}">Calculator</a></li>
     <li><a href="#">Dive Log</a></li>
-    <li><a href="">Blog</a></li>
+    <li><a href="{{ route('blog_list') }}">Blog</a></li>
     @admin
         <ul class="collapsible" data-collapsible="accordian">
             <li>
