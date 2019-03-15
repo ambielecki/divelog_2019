aa5bf41d7df5779c67c86f5e05d5826337fed8c6
Andrew Bielecki <andrew.bielecki@healthcaresource.com>
2019-03-15 14:10:12 -0400

    removed some div-itis

--- resources/js/components/pages/DiveLog.vue
+++ resources/js/components/pages/DiveLog.vue
@@ -4,29 +4,25 @@
             <div class="col s12 m6 l4">
                 <div class="card">
                     <div class="card-content">
-                        <span class="card-title">General Info</span>
+                        <span class="card-title blue-text">General Info</span>
                         <div class="row">
-                            <div class="col s12 m6 l4">
-                                <div class="row">
-                                    <div class="input-field col s12">
-                                        <input id="location" name="location" type="text" v-model="dive_log.location">
-                                        <label for="location">Location: </label>
-                                    </div>
-                                </div>
+                            <div class="input-field col s12">
+                                <input id="location" name="location" type="text" v-model="dive_log.location">
+                                <label for="location">Location: </label>
+                            </div>
+                        </div>
 
-                                <div class="row">
-                                    <div class="input-field col s12">
-                                        <input id="dive_site" name="dive_site" type="text" v-model="dive_log.dive_site">
-                                        <label for="dive_site">Dive Site: </label>
-                                    </div>
-                                </div>
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <input id="dive_site" name="dive_site" type="text" v-model="dive_log.dive_site">
+                                <label for="dive_site">Dive Site: </label>
+                            </div>
+                        </div>
 
-                                <div class="row">
-                                    <div class="input-field col s12">
-                                        <input id="description" name="description" type="text" v-model="dive_log.description">
-                                        <label for="description">Description: </label>
-                                    </div>
-                                </div>
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <input id="description" name="description" type="text" v-model="dive_log.description">
+                                <label for="description">Description: </label>
                             </div>
                         </div>
                     </div>
